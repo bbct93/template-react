f69ca2e22a5248465dc009571131f1fafa394939
chenTao <chentao@xiniaoyun.com>
2019-11-12 17:28:43 +0800

    配置@babel/plugin-proposal-class-properties、@babel/plugin-proposal-decorators  避免类中无法使用es6

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,14 @@ import {render} from "react-dom";
 import "./index.less"
 
 class Greeter extends Component {
+    fn = () => {
+        console.log('this is fn1`');
+    };
     render() {
         return (
             <div>
                 <span className="txt">I am greeter5</span>
+                <button onClick={this.fn}>click here</button>
             </div>
         )
     }
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', {loader: 'css-loader', options: {modules: true}}],
             },
+            // less
             {
                 test: /\.less$/,
                 use: [
