67d19bd2dc2ad373c907b348067139f15cb102df
chenTao <chentao@xiniaoyun.com>
2019-12-03 13:49:11 +0800

    update readme and add webpackBar

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,7 @@
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import WebpackBar from 'webpackbar';
+
 
 module.exports = {
     entry: __dirname + "/src/index.tsx",  //入口文件
@@ -12,7 +14,7 @@ module.exports = {
         contentBase: "./build",// 本地服务器所加载的页面所在的目录 即index.html所在位置
         historyApiFallback: true,
         inline: true,
-        port: 5000
+        port: 5001
     },
     module: {
         rules: [
@@ -63,6 +65,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: __dirname + "/public/index.template.html"
-        })
+        }),
+        new WebpackBar()
     ]
 };
